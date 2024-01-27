247e04d63fb3f5af37a0cb5ff2ff2ddfbaa2032a
Mahmoud Darwish <156988540+Darwish-313@users.noreply.github.com>
2024-01-26 23:00:00 -0800

    Add files via upload

---
+++ SALE.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner input=new Scanner(System.in);
+        int t=input.nextInt();
+        while(t-->0){
+            int a=input.nextInt();
+            int b=input.nextInt();
+            int c=input.nextInt();
+            int [] x={a,b,c};
+            Arrays.sort(x);
+            System.out.println(x[1]+x[2]);
+        }
+	}
+}
